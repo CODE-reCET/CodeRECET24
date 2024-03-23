8fa1e618a893d5ffcad237383b197a3284f7299d
aagneye-syam <saagneye2003@gmail.com>
2024-03-23 10:25:59 +0530

    client module created

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
