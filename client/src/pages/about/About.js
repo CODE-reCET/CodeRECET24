42bc0cfdaedf515f9255c1d54960bf2a3a0e6213
Abhijith B <babhijith58@gmail.com>
2024-03-23 23:18:15 +0000

    about us

---
+++ client/src/pages/about/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Ab } from './Ab.js';
+import Navbare from '../components/Navbar';
+
+function About() {
+    return (
+        <>
+            <Navbare />
+            <Ab />
+        </>
+    );
+}
+
+export default About;
