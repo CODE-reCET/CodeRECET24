147705e9aaee5c55848452c55a3fa6f691174092
Abhimanyu R B <abhimanyurbsa@gmail.com>
2024-03-23 11:07:14 +0530

    added routing - login page

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Login from './pages/login/Login';
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+ } from "react-router-dom";
+  
+ const router = createBrowserRouter([
+   {
+    path: "/",
+    element: <Login />,
+   },
+   {
+     path: "/home",
+     element: <App />,
+   },
+   
+ ]);
+  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-    <App />
-  
+  <RouterProvider router={router} />
+
 );
 
