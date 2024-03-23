654c37708fbb8b7080dd1326744e92b33accbbf4
aagneye-syam <saagneye2003@gmail.com>
2024-03-23 13:32:37 +0530

    logo and backgorund-color added to Login page

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -1,6 +1,7 @@
 // Login.jsx
 import React, { useState } from 'react';
 import './Login.css';
+import logoGif from '../../assets/Logo.gif'; // Adjust the path to Logo.gif
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -13,28 +14,33 @@ const Login = () => {
 
   return (
     <div className="login-container">
-      <div className="form-container">
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button type="submit">Login</button>
-        </form>
+      <div className="sec1">
+        <img src={logoGif} alt="Logo" className="logo" /> {/* Add the GIF as logo */}
       </div>
-      <div className="link-container">
-        <a href="/signup" className="link">Sign Up</a>
-        <a href="/forgot-password" className="link">Forgot Password?</a>
+      <div className="sec2">
+        <div className="form-container">
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">Login</button>
+          </form>
+        </div>
+        <div className="link-container">
+          <a href="/signup" className="link">Sign Up</a>
+          <a href="/forgot-password" className="link">Forgot Password?</a>
+        </div>
       </div>
     </div>
   );
