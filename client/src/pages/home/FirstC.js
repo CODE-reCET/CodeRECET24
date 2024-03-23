ff304088bf17fda8ede4163b46167d0a5e6939a4
Abhijith B <babhijith58@gmail.com>
2024-03-23 23:39:17 +0000

    aboutus content

--- client/src/pages/home/FirstC.js
+++ client/src/pages/home/FirstC.js
@@ -13,16 +13,16 @@ function FirstC() {
           <div className="row gy-3 gy-lg-0 align-items-lg-center justify-content-lg-between">
             <div className="col-12 col-lg-6 order-1 order-lg-0">
               <h1 className="font2 display-3 fw-bolder mb-3">
-                We design sustainable <br />
+                We design Smart <br />
                 <mark className="bsb-tpl-highlight bsb-tpl-highlight-blue">
                   <span
                     className="font -2 bsb-tpl-font-hw display-2 text-accent fw-normal"
                     style={{ fontSize: "50px" }}
                   >
-                    solutions
+                    Solutions
                   </span>
                 </mark>{" "}
-                for Biomedical waste disposal
+                for Biomedical waste disposal.
               </h1>
               <p className="fs-4 mb-5">
                 "Stay safe from infections by using our innovative web app solution."
@@ -37,7 +37,8 @@ function FirstC() {
                 </button>
                 <button
                   type="button"
-                  className="btn2 btn btn-outline-primary bsb-btn-3xl rounded-pill"
+                  className="btn2 btn btn-outline-success bsb-btn-3xl rounded-pill"
+                  
                 >
                   Services
                 </button>
