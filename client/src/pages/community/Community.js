8223c0e3ba2ebd38c2ff0f22ad5359d3022dfeaa
aagneye-syam <saagneye2003@gmail.com>
2024-03-24 03:05:38 +0530

    community child component got display flex

--- client/src/pages/community/Community.js
+++ client/src/pages/community/Community.js
@@ -2,14 +2,20 @@ import React from "react";
 import Navbare from "../../components/Navbar";
 import MembershipStatus from "./membership/Membership";
 import Donation from "./donation/Donation";
+import './Community.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Community() {
   return (
     <>
       <Navbare />
-      <div className="container d-flex mt-5 justify-content-center">
-        <MembershipStatus />
-        <Donation/>
+      <div className="community community-container">
+        <div>
+          <MembershipStatus />
+        </div>
+        <div>
+          <Donation />
+        </div>
       </div>
     </>
   );
