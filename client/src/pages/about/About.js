721daebecf48cdfa80dc3ce7663ed79b51e68c06
Abhimanyu R B <abhimanyurbsa@gmail.com>
2024-03-24 05:30:17 +0530

    stylized webpage

--- client/src/pages/about/About.js
+++ client/src/pages/about/About.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbare from "../../components/Navbar.js";
 import BinTrackingDashboard from "../BinTracking/BinTrack.js";
+import MapComponent from "../map/MapComponent.js";
 
 function Bin() {
   return (
     <>
       <Navbare />
       <BinTrackingDashboard />
+      <MapComponent />
     </>
   );
 }
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -2,14 +2,12 @@ import React from "react";
 import Navbar from "../../components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import FirstC from "./FirstC";
-import MapComponent from "../map/MapComponent";
 
 function Home() {
   return (
     <>
       <Navbar />
       <FirstC />
-      <MapComponent />
     </>
   );
 }
